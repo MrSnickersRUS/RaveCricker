21e8a869773aba8215d058337af80efeb19b4232
d4shsk <75478355+d4shsk@users.noreply.github.com>
2022-12-11 23:18:44 +0700

    Button Held

---
+++ Assets/Scripts/ItemDestroy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroy : MonoBehaviour
+{
+    [SerializeField] private float itemBreakTime;
+    private float currentTime;
+
+    private bool isHeld;
+
+    private void Start()
+    {
+        currentTime = itemBreakTime;
+    }
+
+    public void OnButtonDown() {
+        currentTime = 0;
+        isHeld = true;
+        StartCoroutine(Breaking());
+    }
+
+    public void OnButtonUp() {
+        isHeld = false;
+    }
+
+    private IEnumerator Breaking() {
+        while (isHeld) {
+            currentTime += Time.deltaTime;
+            if ((int) currentTime == itemBreakTime) {
+                print("broken");
+            }
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}

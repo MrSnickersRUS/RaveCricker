7e6ec61c1630531b13349dd3ad452e28f1512193
MrSnickersRUS <nikitadmitriev9897@gmail.com>
2022-12-15 00:20:38 +0700

    best update with errors :D

--- Assets/Scripts/ItemDestroy.cs
+++ Assets/Scripts/ItemDestroy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Random;
 
 public class ItemDestroy : MonoBehaviour
 {
@@ -8,9 +9,17 @@ public class ItemDestroy : MonoBehaviour
 
     private bool isHeld;
 
+    private Random randomNum;
+    [SerializeField] private Random randomChance;
+
+    private int dirtNum;
+
+    private int GravelNum;
+
     private void Start()
     {
         currentTime = itemBreakTime;
+        dirtNum = 0;
     }
 
     public void OnButtonDown() {
@@ -29,8 +38,32 @@ public class ItemDestroy : MonoBehaviour
             if ((int) currentTime == itemBreakTime) {
                 print("broken");
                 Destroy(gameObject);
+                DirtGiver;
+                GravelGiver;
             }
             yield return new WaitForFixedUpdate();
+        }
+    }
+
+    void DirtGiver()
+    {
+        int dirtGiver = new Random randomNum.Next(1, 5)
+        dirtNum += dirtGiver;
+        PlayerPrefs.SetInt("Dirt number", dirtNum);
+
+        print("получено земля");
+    }
+
+    void GravelGiver()
+    {
+        int DropChance = new Random randomChance.Next(1, 100);
+        if (DropChance > 40)
+        {
+            int GravelGiver = new Random randomNum.Next(0, 1);
+            GravelNum += GravelGiver;
+            PlayerPrefs.SetInt("Gravel number", gravelNum);
+
+            print("получено гравий");
         }
     }
 }
